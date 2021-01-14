bd0ea38a266beab109df8dc5ff5f5f66101d09dc
YuhSylphy <yuhsylphy@gmail.com>
2021-01-14 09:00:58 +0900

    experimental

--- src/features/family/Family.tsx
+++ src/features/family/Family.tsx
@@ -8,13 +8,13 @@ type LinkDatum = d3.SimulationLinkDatum<NodeDatum>;
 
 const FamilyDiagram: React.FC = () => {
     const nodes = useMemo<NodeDatum[]>(() => [
-        { id: 0, depth: 0, label: 'foo' },
-        { id: 1, depth: 1, label: 'bar' },
-        { id: 2, depth: 0, label: 'buz' },
-        { id: 3, depth: 2, label: 'qux' },
-        { id: 4, depth: 3, label: 'x' },
-        { id: 5, depth: 3, label: 'y' },
-        { id: 6, depth: 4, label: 'z' }
+        { id: 0, depth: 1, label: 'foo' },
+        { id: 1, depth: 2, label: 'bar' },
+        { id: 2, depth: 3, label: 'buz' },
+        { id: 3, depth: 4, label: 'qux' },
+        { id: 4, depth: 5, label: 'x' },
+        { id: 5, depth: 6, label: 'y' },
+        { id: 6, depth: 7, label: 'z' }
     ], []);
 
     const edges = useMemo<LinkDatum[]>(() => [
@@ -44,56 +44,79 @@ const FamilyDiagram: React.FC = () => {
 
         // ノード(点)の設定。
         const d3Nodes = d3Svg
-            .selectAll("circle")
+            .selectAll("rect")
             .data(nodes)
             .enter()
-            .append("circle")
-            .attr("r", 10)
+            .append("rect")
+            .attr("width", 128)
+            .attr("height", 32)
             .attr("fill", "#213964")
             .call(
                 d3
-                    .drag<SVGCircleElement, NodeDatum>()
-                    .on("start", (event, d) => {
-                        if (!event.active) simulation.alphaTarget(0.9).restart();
-                        d.fx = d.x;
-                        d.fy = d.y;
-                    })
+                    .drag<SVGRectElement, NodeDatum>()
+                    // .on("start", (event, d) => {
+                    //     if (!event.active) simulation.alphaTarget(0.9).restart();
+                    //     d.fx = d.x;
+                    //     d.fy = d.y;
+                    // })
                     .on("drag", (event, d) => {
                         d.fx = event.x;
                         d.fy = event.y;
                     })
-                    .on("end", (event, d) => {
-                        if (!event.active) simulation.alphaTarget(0);
-                        d.fx = null;
-                        d.fy = null;
-                    })
+                // .on("end", (event, d) => {
+                //     if (!event.active) simulation.alphaTarget(0);
+                //     d.fx = null;
+                //     d.fy = null;
+                // })
             );
 
-        const select = (selector: (d: LinkDatum) => NodeDatum | string | number) => 
-            (func: (s: NodeDatum) => string | number | undefined) => 
-            (d: LinkDatum): string | number => {
-                const selected = selector(d);
-                return typeof selected === "object" ? func(selected) || 0 : selected;
-            }
+        const d3Labels = d3Svg
+            .selectAll("text")
+            .data(nodes)
+            .enter()
+            .append('text')
+            .attr("text-anchor", "middle")
+            .attr("fill", "white")
+            .attr("font-size", "16px")
+            .text(d => d.label);
+
+        const select = (selector: (d: LinkDatum) => NodeDatum | string | number) =>
+            (func: (s: NodeDatum) => string | number | undefined) =>
+                (d: LinkDatum): string | number => {
+                    const selected = selector(d);
+                    return typeof selected === "object" ? func(selected) || 0 : selected || 0;
+                }
 
         // 描画とシミュレーションの設定。
         const simulation = d3
             .forceSimulation<NodeDatum>()
             .nodes(nodes)
             .on("tick", () => {
-                d3Nodes.attr("cx", d => d.x || 0).attr("cy", d => d.y || 0);
-                
+                d3Nodes
+                    .attr("x", d => (d.x || 0) - 64)
+                    .attr("y", d => (d.y || 0) - 16);
+
                 d3Links
                     .attr("x1", select(d => d.source)(s => s.x))
                     .attr("y1", select(d => d.source)(s => s.y))
                     .attr("x2", select(d => d.target)(s => s.x))
                     .attr("y2", select(d => d.target)(s => s.y));
+
+                d3Labels
+                    .attr("x", d => (d.x || 0))
+                    .attr("y", d => (d.y || 0) + 8);
             })
-            .force("link", d3.forceLink(edges).distance(50))
-            .force("charge", d3.forceManyBody().strength(-50))  // 斥力。負にすることで反発力が働く
-            .force("center", d3.forceCenter(width / 2, height / 2))
+            .force("link", d3.forceLink(edges).distance(200))
+            .force("charge", d3.forceManyBody().strength(-200))  // 斥力。負にすることで反発力が働く
+            .force("center", d3.forceCenter(width / 2, height / 2).strength(0.2))
             .force("x", d3.forceX())
-            .force("y", d3.forceY());
+            .force("y", d3.forceY())
+            .force("collide",
+                d3.forceCollide()
+                    .radius(function (d) { return Math.max(d.x || 0, d.y || 0); })
+                    .strength(1.0)
+                    .iterations(16)
+            );
 
         // 配置の初期設定。データのdepthに合わせてyを決める
         simulation.nodes().forEach(d => {
