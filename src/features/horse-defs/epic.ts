b4dc365f3756ba4c2dd890af98780370e0964dce
YuhSylphy <yuhsylphy@gmail.com>
2021-01-15 11:03:20 +0900

    Export defs to json

--- src/features/horse-defs/epic.ts
+++ src/features/horse-defs/epic.ts
@@ -1,115 +1,16 @@
 import { Epic, combineEpics } from "redux-observable";
-import { map } from "rxjs/operators";
+import { mergeMap } from "rxjs/operators";
 
 import { horseDefsActions, HorseDefsAction } from ".";
-import { HorseDef } from "./core/horse";
+import { fetchHorseDefs } from "./core/horse";
 
 export const fetchDefsEpic: Epic<HorseDefsAction> = (action$) =>
-  action$.ofType(horseDefsActions.init.type).pipe(
-    map((_) => {
-      // 最終的にはfetchする
-      return horseDefsActions.set({ list: dummyDefs });
-    })
-  );
+  action$
+    .ofType(horseDefsActions.init.type)
+    .pipe(
+      mergeMap(async () =>
+        horseDefsActions.set({ list: await fetchHorseDefs() })
+      )
+    );
 
 export const epic = combineEpics(fetchDefsEpic);
-
-const dummyDefs: HorseDef[] = [
-  { name: "ロズウェルリポート", sex: "female" },
-  {
-    name: "モッティサイキョウ",
-    sex: "male",
-    fatherName: "パイロ",
-    motherName: "ロズウェルリポート",
-  },
-  {
-    name: "ステキマリオクン",
-    sex: "male",
-    fatherName: "ゴールドアリュール",
-    motherName: "ロズウェルリポート",
-  },
-  {
-    name: "ゴマプリン",
-    sex: "female",
-    fatherName: "ジョーカプチーノ",
-    motherName: "ロズウェルリポート",
-  },
-  {
-    name: "ミタラシダンゴ",
-    sex: "female",
-    fatherName: "ネオユニヴァース",
-    motherName: "ロズウェルリポート",
-  },
-  {
-    name: "カントリーマアム",
-    sex: "female",
-    fatherName: "マンハッタンカフェ",
-    motherName: "ロズウェルリポート",
-  },
-  {
-    name: "ルマンド",
-    sex: "female",
-    fatherName: "ダイワメジャー",
-    motherName: "ロズウェルリポート",
-  },
-  {
-    name: "モリナガノコエダ",
-    sex: "female",
-    fatherName: "ジョーカプチーノ",
-    motherName: "ロズウェルリポート",
-  },
-  {
-    name: "コーヒーゼリィ",
-    sex: "male",
-    fatherName: "ジョーカプチーノ",
-    motherName: "ロズウェルリポート",
-  },
-  {
-    name: "スウィートポテト",
-    sex: "female",
-    fatherName: "エンパイアメーカー",
-    motherName: "ゴマプリン",
-  },
-  {
-    name: "ムギチャ",
-    sex: "female",
-    fatherName: "ジョーカプチーノ",
-    motherName: "ロズウェルリポート",
-  },
-  {
-    name: "オレノオレオクッキ",
-    sex: "male",
-    fatherName: "ゴールドシップ",
-    motherName: "ゴマプリン",
-  },
-  {
-    name: "ペコチャン",
-    sex: "female",
-    fatherName: "ルーラーシップ",
-    motherName: "カントリーマアム",
-  },
-  {
-    name: "ピノ",
-    sex: "female",
-    fatherName: "アグネスデジタル",
-    motherName: "ゴマプリン",
-  },
-  {
-    name: "カブキアゲ",
-    sex: "male",
-    fatherName: "オルフェーブル",
-    motherName: "カントリーマアム",
-  },
-  {
-    name: "アルフォート",
-    sex: "male",
-    fatherName: "アグネスデジタル",
-    motherName: "カントリーマアム",
-  },
-  {
-    name: "ジャイアントコーン",
-    sex: "male",
-    fatherName: "ケープブランコ",
-    motherName: "ミタラシダンゴ",
-  },
-];
