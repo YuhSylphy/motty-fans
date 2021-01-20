e2dd9ba40d29db50b99255de5da804d39b22b803
YuhSylphy <yuhsylphy@gmail.com>
2021-01-20 18:50:43 +0900

    Experiments for markdown

--- src/features/changelog/components/ChangeLog.tsx
+++ src/features/changelog/components/ChangeLog.tsx
@@ -1,14 +1,26 @@
-import * as React from "react";
-
 import { Paper, Typography } from "@material-ui/core";
 
-import {} from "react-markdown";
+import * as React from "react";
+import Markdown from "react-markdown";
+
+const markdown = `
+# Header
+
+## section
+
+* A
+* B
+  * B-1
+  * **B-2**
+`;
 
 export const ChangeLog: React.FC = () => {
   return (
     <React.Fragment>
       <Typography variant="h4">更新</Typography>
-      <Paper>履歴</Paper>
+      <Paper>
+        <Markdown source={markdown} />
+      </Paper>
     </React.Fragment>
   );
 };
