ae10080a595154a2b1fd8ae932fccc4c9cad3dd0
YuhSylphy <yuhsylphy@gmail.com>
2021-01-21 19:30:30 +0900

    Apply github-markdown-css to markdown component

--- src/features/changelog/components/ChangeLog.tsx
+++ src/features/changelog/components/ChangeLog.tsx
@@ -1,7 +1,9 @@
-import { Paper, Typography } from "@material-ui/core";
+import { Paper, Typography, createStyles, makeStyles } from "@material-ui/core";
 
 import * as React from "react";
 import Markdown from "react-markdown";
+
+import "github-markdown-css";
 
 const markdown = `
 # Header
@@ -14,12 +16,24 @@ const markdown = `
   * **B-2**
 `;
 
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    paper: {
+      padding: theme.spacing(1),
+    },
+  })
+);
+
 export const ChangeLog: React.FC = () => {
+  const classes = useStyles();
   return (
     <React.Fragment>
       <Typography variant="h4">更新</Typography>
-      <Paper>
+      <Paper className={classes.paper}>
         <Markdown source={markdown} />
+      </Paper>
+      <Paper className={classes.paper}>
+        <Markdown source={markdown} className="markdown-body" />
       </Paper>
     </React.Fragment>
   );
