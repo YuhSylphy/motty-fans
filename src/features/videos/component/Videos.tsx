02e4fe82f52fc9c7ed4e04db34d52f3d8382441f
YuhSylphy <yuhsylphy@gmail.com>
2022-04-18 18:22:30 +0900

    List videos with Cards

--- src/features/videos/component/Videos.tsx
+++ src/features/videos/component/Videos.tsx
@@ -1,4 +1,19 @@
-import React, { Suspense, useEffect } from 'react';
+import React, { Suspense, useEffect, useMemo } from 'react';
+
+import {
+	Button,
+	Card,
+	CardActions,
+	CardContent,
+	CardMedia,
+	Tooltip,
+	Typography,
+} from '@mui/material';
+import { Masonry } from '@mui/lab';
+import { styled } from '@mui/styles';
+
+import { DateTime } from 'luxon';
+
 import { useAppDispatch, useAppSelector } from 'src/util';
 import { videosActions } from '..';
 import { VideoDef } from '../core/logic';
@@ -7,22 +22,104 @@ function Loader() {
 	return <div>Loading...</div>;
 }
 
-type VideoProps = {
+type VideoCardProps = {
+	def: VideoDef;
+};
+
+const TitleStyledTypography = styled(Typography)({
+	display: 'box',
+	overflow: 'hidden',
+	lineClamp: 2,
+	boxOrient: 'vertical',
+});
+
+function TitleTypography(props: React.ComponentProps<typeof Typography>) {
+	return (
+		<Tooltip title={props.children ?? ''}>
+			<TitleStyledTypography {...props} gutterBottom variant="h6" />
+		</Tooltip>
+	);
+}
+
+function PublishedAtTypography(props: React.ComponentProps<typeof Typography>) {
+	return (
+		<Tooltip title={props.children ?? ''}>
+			<Typography
+				{...props}
+				variant="body2"
+				color="text.secondary"
+				align="right"
+			/>
+		</Tooltip>
+	);
+}
+
+const DescriptionStyledTypography = styled(Typography)({
+	display: 'box',
+	overflow: 'hidden',
+	lineClamp: 3,
+	boxOrient: 'vertical',
+});
+
+function DescriptionTypography(props: React.ComponentProps<typeof Typography>) {
+	return (
+		<Tooltip title={props.children ?? ''}>
+			<DescriptionStyledTypography {...props} variant="body2" />
+		</Tooltip>
+	);
+}
+
+type LinkToMovieButtonProps = {
+	id: string;
+};
+function LinkToMovieButton({ id }: LinkToMovieButtonProps) {
+	return (
+		<Button href={`https://www.youtube.com/watch?v=${id}`} target="_blank">
+			Watch
+		</Button>
+	);
+}
+
+const RightAlignedCardActions = styled(CardActions)({
+	justifyContent: 'right',
+});
+
+function VideoCard({ def }: VideoCardProps) {
+	const thumb = def.thumbnails.default;
+	const publishedAt = useMemo(
+		() =>
+			DateTime.fromMillis(def.publishedAt)
+				.setZone('Asia/Tokyo')
+				.toFormat('yyyy-MM-dd HH:mm:ss'),
+		[def.publishedAt]
+	);
+	return (
+		<Card>
+			<CardMedia component="img" image={thumb.url} alt={def.title} />
+			<CardContent>
+				<TitleTypography>{def.title}</TitleTypography>
+				<PublishedAtTypography>{publishedAt}</PublishedAtTypography>
+				<DescriptionTypography>{def.description}</DescriptionTypography>
+			</CardContent>
+			<RightAlignedCardActions>
+				<LinkToMovieButton id={def.id} />
+			</RightAlignedCardActions>
+		</Card>
+	);
+}
+
+type VideoBodyProps = {
 	defs: VideoDef[];
 };
 
-function VideoBody({ defs }: VideoProps) {
+function VideoBody({ defs }: VideoBodyProps) {
 	// tentative
 	return (
-		<div>
-			<ul>
-				{defs.map((def, ix) => (
-					<li key={ix}>
-						<pre>{JSON.stringify(def, null, '\t')}</pre>
-					</li>
-				))}
-			</ul>
-		</div>
+		<Masonry>
+			{defs.map((def) => (
+				<VideoCard key={def.id} def={def} />
+			))}
+		</Masonry>
 	);
 }
 
--- src/features/videos/core/logic.ts
+++ src/features/videos/core/logic.ts
@@ -1,3 +1,5 @@
+import { DateTime } from 'luxon';
+
 type YouTubeId = {
 	kind: 'youtube#video';
 	videoId: string;
@@ -39,26 +41,26 @@ type JsonType = {
 
 const dummy: VideoDef = {
 	id: 'dummy',
-	publishedAt: Date.parse('2014-09-07T00:00:00Z'),
+	publishedAt: DateTime.fromISO('2014-09-07T00:00:00Z').toMillis(),
 	title: 'dummy',
 	description: 'no videos fetched',
-	// thumbnails: {
-	// 	default: {
-	// 		url: 'dummy',
-	// 		width: 120,
-	// 		height: 90,
-	// 	},
-	// 	medium: {
-	// 		url: 'dummy',
-	// 		width: 320,
-	// 		height: 180,
-	// 	},
-	// 	high: {
-	// 		url: 'dummy',
-	// 		width: 480,
-	// 		height: 360,
-	// 	},
-	// },
+	thumbnails: {
+		default: {
+			url: 'dummy',
+			width: 120,
+			height: 90,
+		},
+		medium: {
+			url: 'dummy',
+			width: 320,
+			height: 180,
+		},
+		high: {
+			url: 'dummy',
+			width: 480,
+			height: 360,
+		},
+	},
 	tags: [],
 };
 
@@ -67,22 +69,28 @@ export type VideoDef = {
 	publishedAt: number;
 	title: string;
 	description: string;
-	// thumbnail:
+	thumbnails: Thumbnails;
 	tags: string[];
 };
 
 export const fetchVideoDefs = (): Promise<VideoDef[]> =>
 	fetch(`${process.env.PUBLIC_URL}/assets/videos/videos.json`)
 		.then((res) => res.json())
-		.then(({ items }: JsonType) =>
-			!(items && items.length > 0)
-				? [dummy]
-				: items.map((def) => ({
-						id: def.id.videoId,
-						publishedAt: Date.parse(def.snippet.publishedAt),
-						title: def.snippet.title,
-						description: def.snippet.description,
-						tags: [],
-				  }))
+		.then(
+			({ items }: JsonType) =>
+				!(items && items.length > 0)
+					? [dummy]
+					: items
+							.map((def) => ({
+								id: def.id.videoId,
+								publishedAt: DateTime.fromISO(
+									def.snippet.publishedAt
+								).toMillis(),
+								title: def.snippet.title,
+								description: def.snippet.description,
+								thumbnails: def.snippet.thumbnails,
+								tags: [],
+							}))
+							.slice(0, 20) // tentative
 		)
 		.catch((e) => [{ ...dummy, description: e }]);
