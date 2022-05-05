5be690c981955402df14b6d9b2e1043fdb8221d5
YuhSylphy <yuhsylphy@gmail.com>
2022-05-05 19:56:58 +0900

    Set default date span

--- src/features/videos/core/epic.ts
+++ src/features/videos/core/epic.ts
@@ -1,6 +1,8 @@
 import { combineEpics } from 'redux-observable';
 import { filter } from 'rxjs/operators';
 
+import { DateTime } from 'luxon';
+
 import type { Epic } from 'src/app';
 import { withIndicator } from 'src/util';
 
@@ -23,4 +25,18 @@ export const calcTagCandidatesEpic: Epic = (action$) =>
 		)
 	);
 
-export const epic = combineEpics(fetchDefsEpic, calcTagCandidatesEpic);
+export const setDefaultConditionEpic: Epic = (action$) =>
+	action$.pipe(
+		filter(videosActions.init.match),
+		withIndicator('videos/default-condition', async () =>
+			videosActions.setConditionDateFrom(
+				DateTime.now().minus({ months: 3 }).toMillis()
+			)
+		)
+	);
+
+export const epic = combineEpics(
+	fetchDefsEpic,
+	calcTagCandidatesEpic,
+	setDefaultConditionEpic
+);
