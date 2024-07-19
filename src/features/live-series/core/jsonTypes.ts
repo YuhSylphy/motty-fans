c773ac969c41369a7a45c4e95c86a7b6e046b40e
YuhSylphy <yuhsylphy@gmail.com>
2024-07-19 22:12:13 +0900

    feat: Define the other json types

--- src/features/live-series/core/jsonTypes.ts
+++ src/features/live-series/core/jsonTypes.ts
@@ -10,3 +10,29 @@ export type LiveDef = {
 export type LivesJson = {
 	items: LiveDef[];
 };
+
+export type LiveSeriesDef = {
+	id: string;
+	seriesTitle: string;
+	tags: string[];
+	gameId: string | null;
+};
+
+export type LiveSeriesJson = {
+	items: LiveSeriesDef[];
+};
+
+export type MasteryLevel = 0 | 1 | 2 | 3 | 4 | 5;
+
+export type GamesDef = {
+	id: string;
+	gameTitle: string;
+	platform: string | null;
+	tags: string[];
+	releasedIn: number | string | null;
+	masteryLevel: MasteryLevel;
+};
+
+export type GamesJson = {
+	items: GamesDef[];
+};
