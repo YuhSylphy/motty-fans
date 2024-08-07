8aaa846b7ff47923e9b765fd7caa2dbe91f9334f
YuhSylphy <yuhsylphy@gmail.com>
2024-08-07 13:42:39 +0000

    Rebuild docs by Actions

---
+++ docs/assets/useThemeProps-BgSxuZIJ.js
@@ -0,0 +1,1 @@
+import{R as m,l as o}from"./menu-defs-zBJLu_tM.js";import{u as s}from"./IconButton-CMST3W7o.js";function a({props:e,name:r}){return s({props:e,name:r,defaultTheme:m,themeId:o})}export{a as u};
