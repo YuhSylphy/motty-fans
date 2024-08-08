3797faec81def17f404348401c07924a56d7e041
YuhSylphy <yuhsylphy@gmail.com>
2024-08-08 12:43:09 +0900

    fix: Factor out providers from NavFrame

--- app/core/components/NavFrame.tsx
+++ app/core/components/NavFrame.tsx
@@ -1,8 +1,5 @@
-import { Box, CssBaseline } from '@mui/material';
+import { Box } from '@mui/material';
 import { styled } from '@mui/material/styles';
-import { AdapterLuxon } from '@mui/x-date-pickers/AdapterLuxon';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-
 import React, { Suspense, useEffect } from 'react';
 import { Outlet } from '@remix-run/react';
 
@@ -13,38 +10,31 @@ import { Indicator } from '~/features/indicator';
 import { PedigreeDialog } from '~/features/pedigree';
 
 import './NavFrame.css';
-import { AppThemeProvider } from './AppThemeProvider';
 import { Footer } from './Footer';
 import { Header } from './Header';
-import { BrowserRouter } from 'react-router-dom';
 
 const AppBox = styled(Box)(({ theme }) => ({
 	margin: theme.spacing(0.5),
 }));
 
-export function NavFrame() {
+export const useNavFrameHooks = () => {
 	useEffect(() => {
 		registerEpic(exportEpic);
 	}, []);
+};
 
+export function NavFrame() {
+	useNavFrameHooks();
 	return (
-		<Suspense fallback={<Indicator />}>
-			<LocalizationProvider dateAdapter={AdapterLuxon}>
-				<AppThemeProvider>
-					<BrowserRouter basename={import.meta.env.BASE_URL}>
-						<CssBaseline />
-						<Header />
-						<AppBox>
-							<Suspense fallback={<Indicator />}>
-								<Outlet />
-							</Suspense>
-						</AppBox>
-						<Footer />
-						<PedigreeDialog />
-						<Indicator />
-					</BrowserRouter>
-				</AppThemeProvider>
-			</LocalizationProvider>
-		</Suspense>
+		<React.Fragment>
+			<Header />
+			<AppBox>
+				<Suspense fallback={<Indicator />}>
+					<Outlet />
+				</Suspense>
+			</AppBox>
+			<Footer />
+			<PedigreeDialog />
+		</React.Fragment>
 	);
 }
